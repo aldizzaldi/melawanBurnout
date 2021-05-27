4054a3b68d34712a63cc868fbda993bd22d942dc
rosyidahadilah <79310760+rosyidahadilah@users.noreply.github.com>
2021-05-27 13:36:56 +0700

    detail discover

---
+++ app/src/main/java/com/example/capstone/ui/discover/DetailDiscover.kt
@@ -0,0 +1,12 @@
+package com.example.capstone.ui.discover
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstone.R
+
+class DetailDiscover : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_discover)
+    }
+}
